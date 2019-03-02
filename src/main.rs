be33398539bc2a42eb439b9bc72d8958cb8c8e8c
phreppo <parolini.francesco.1@gmail.com>
2019-03-02 15:41:37 +0100

    removed parser tests from main

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,6 @@ use leesp::lispcore::*;
 use leesp::language::*;
 use leesp::parser::*;
 
-
-#[test]
-fn lisp_parser() {
-    let result = parse("NIL");
-    assert_eq!(result, new_nil());
-
-    let result = parse("2");
-    assert_eq!(result, new_num(2));
-
-    let result = parse("-2");
-    assert_eq!(result, new_num(-2));
-
-    let result = parse("car");
-    assert_eq!(result, new_symbol("car".to_string()));
-
-    let result = parse("car");
-    assert_eq!(result, new_symbol("CAR".to_string()));
-
-    let result = parse("sTrAnGeVaR123");
-    assert_eq!(result, new_symbol("STRANGEVAR123".to_string()));
-
-    let result = parse("'string'");
-    assert_eq!(result, new_str("'string'".to_string()));
-
-    let result = parse("(ciao . NIL)");
-    assert_eq!(
-        result,
-        new_cons(Rc::new(new_symbol("ciao".to_string())), Rc::new(new_nil()))
-    );
-
-    // let result = leesp::parser::
-    //     .parse("(ciao . (1 . NIL))")
-    //     .unwrap();
-    // assert_eq!(
-    //     result,
-    //     new_cons(
-    //         Rc::new(new_symbol("ciao".to_string())),
-    //         Rc::new(new_cons(Rc::new(new_num(1)), Rc::new(new_nil())))
-    //     )
-    // );
-}
-
 fn repl() {
     loop {
         print!("> ");
--- src/parser.rs
+++ src/parser.rs
@@ -5,3 +5,44 @@ pub fn parse(s: &str) -> Cell {
     // TODO: permettere di recuperare i parsing errors
     SexprParser::new().parse(s).unwrap()
 }
+
+#[test]
+fn lisp_parser() {
+    use std::rc::Rc;
+
+    let result = parse("NIL");
+    assert_eq!(result, new_nil());
+
+    let result = parse("2");
+    assert_eq!(result, new_num(2));
+
+    let result = parse("-2");
+    assert_eq!(result, new_num(-2));
+
+    let result = parse("car");
+    assert_eq!(result, new_symbol("car".to_string()));
+
+    let result = parse("car");
+    assert_eq!(result, new_symbol("CAR".to_string()));
+
+    let result = parse("sTrAnGeVaR123");
+    assert_eq!(result, new_symbol("STRANGEVAR123".to_string()));
+
+    let result = parse("'string'");
+    assert_eq!(result, new_str("string".to_string()));
+
+    let result = parse("(ciao . NIL)");
+    assert_eq!(
+        result,
+        new_cons(Rc::new(new_symbol("ciao".to_string())), Rc::new(new_nil()))
+    );
+
+    let result = parse("(ciao . (1 . NIL))");
+    assert_eq!(
+        result,
+        new_cons(
+            Rc::new(new_symbol("ciao".to_string())),
+            Rc::new(new_cons(Rc::new(new_num(1)), Rc::new(new_nil())))
+        )
+    );
+}
